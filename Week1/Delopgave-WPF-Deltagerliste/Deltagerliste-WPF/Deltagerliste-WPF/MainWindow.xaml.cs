d1c87d4f45118bbdf9ecb8601d864e762f60cf3a
christoffer jakobsen <christoffer.k.jakobsen@gmail.com>
2018-08-30 09:23:16 +0200

    Week 1 lab added

---
+++ Week1/Delopgave-WPF-Deltagerliste/Deltagerliste-WPF/Deltagerliste-WPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Deltagerliste_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void DelList_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\chris\OneDrive\Desktop\4. Semester\I4GUI\i4gui\Week1\Delopgave-WPF-Deltagerliste\Deltagerliste-WPF\Deltagerliste-WPF\Files\02 deltagerliste.csv");
+            var data = lines.Skip(1);
+            DelList_Label.Content = data;
+        }
+    }
+}
---
+++ Week1/Delopgave3-Week1/Delopgave3-Week1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delopgave3_Week1
+{
+    class Program
+    {
+
+   
+    static void Main(string[] args)
+        {
+            int Number1;
+            int Number2;
+            int sum; 
+
+            Console.WriteLine("Enter first Number: ");
+
+            var UserInput1 = Console.ReadKey();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter second Number: ");
+
+            var UserInput2 = Console.ReadKey();
+        
+            Console.WriteLine("\n");
+
+           
+                Number1 = int.Parse(UserInput1.KeyChar.ToString());
+                Console.WriteLine("Number 1 is: " + Number1); 
+          
+
+           
+                Number2 = int.Parse(UserInput2.KeyChar.ToString());
+                Console.WriteLine("Number 2 is: " + Number2);
+
+
+            sum = Number1 + Number2;
+            Console.WriteLine("Summen er:" + sum); 
+
+        }
+    }
+}
